f5e044795eb9ca0f878a7a6a2e05bdb9c28108a6
ImRex_ <46315281+1mR3x@users.noreply.github.com>
2023-07-07 12:47:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,37 +1,42 @@
-import hashlib, argparse, time
-from colour import Color
-from PIL import ImageColor
+##############################################################################################
 
-class textUtil:
-    RESET = '\033[0m'
+import hashlib, time, os, shutil
+from colorama import Fore
 
-    def printg(text, c1, c2):
-        if text == "":
-            print()
-            return ""
-        else:
-            def get_color_escape(r, g, b, background=False):
-                return '\033[{};2;{};{};{}m'.format(48 if background else 38, r, g, b)
+words = set()
 
-            def get_rgb_code(hex):
-                return ImageColor.getcolor(hex, "RGB")
+##############################################################################################
 
-            c1 = Color(c1)
-            c2 = Color(c2)
-            lt = len(text)
-            cl = list(c1.range_to(c2,lt))
-            col=0
-            ft = ""
-            for i in cl:
-                i = str(get_rgb_code(str(i))).replace('(', '').replace(')', '').split(', ')
-                i = get_color_escape(i[0], i[1], i[2])
-                ft+=f"{i}{text[col]}"
-                col+=1
-            ft+=textUtil.RESET
-            return ft
+logo = """
+·▄▄▄▄  ▄▄▄ . ▄ .▄ ▄▄▄· .▄▄ ·  ▄ .▄▄▄▄ .▄▄▄  
+██▪ ██ ▀▄.▀·██▪▐█▐█ ▀█ ▐█ ▀. ██▪▐█▀▄.▀·▀▄ █·
+▐█· ▐█▌▐▀▀▪▄██▀▐█▄█▀▀█ ▄▀▀▀█▄██▀▐█▐▀▀▪▄▐▀▀▄ 
+██. ██ ▐█▄▄▌██▌▐▀▐█ ▪▐▌▐█▄▪▐███▌▐▀▐█▄▄▌▐█•█▌
+▀▀▀▀▀•  ▀▀▀ ▀▀▀ · ▀  ▀  ▀▀▀▀ ▀▀▀ · ▀▀▀ .▀  ▀
+"""
+error = """
+▄▄▄ .▄▄▄  ▄▄▄        ▄▄▄  
+▀▄.▀·▀▄ █·▀▄ █·▪     ▀▄ █·
+▐▀▀▪▄▐▀▀▄ ▐▀▀▄  ▄█▀▄ ▐▀▀▄ 
+▐█▄▄▌▐█•█▌▐█•█▌▐█▌.▐▌▐█•█▌
+ ▀▀▀ .▀  ▀.▀  ▀ ▀█▄▀▪.▀  ▀
+"""
+
+##############################################################################################
+
+def printcenter(text):
+    size = shutil.get_terminal_size().columns
+    for line in text.split("\n"):
+        print(' ' * (round((size/2)-len(line)/2)), line)
+
+##############################################################################################
 
 def bruteforce(hash, salt):
-    if len(hash) == 86 or len(hash) == 85: # SHA256 - AuthMe - 1
+    if len(hash) == 64:
+        for password in words:
+            if hashlib.sha256(password.encode()).hexdigest() == hash:
+                return password
+    if len(hash) == 86 or len(hash) == 85:
         salt1 = hash.split("$")[2]
         hash1 = hash.split("$")[3]
         for word in words:
@@ -39,7 +44,7 @@ def bruteforce(hash, salt):
             final = hashlib.sha256(var2.encode() + salt1.encode()).hexdigest()
             if final == hash1:
                 return word
-    if len(hash) == 128: # SHA512 - DBA
+    if len(hash) == 128:
         for word in words:
             var2 = hashlib.sha512(word.encode()).hexdigest()
             final = hashlib.sha512((var2 + salt).encode()).hexdigest()
@@ -53,76 +58,67 @@ def bruteforce(hash, salt):
                 return word
     return hash
 
-words = []
-parser = argparse.ArgumentParser(description='Dictionary requirement')
-parser.add_argument('-f', '--file', type=str, help='Route to dictionary.')
-args = parser.parse_args()
-file = args.file
-for _ in range(100): print()
+##############################################################################################
 
-logo = """██╗      ██████╗  █████╗ ██████╗ ██╗███╗   ██╗ ██████╗     ██████╗ ██╗ ██████╗████████╗██╗ ██████╗ ███╗   ██╗ █████╗ ██████╗ ██╗   ██╗
-██║     ██╔═══██╗██╔══██╗██╔══██╗██║████╗  ██║██╔════╝     ██╔══██╗██║██╔════╝╚══██╔══╝██║██╔═══██╗████╗  ██║██╔══██╗██╔══██╗╚██╗ ██╔╝
-██║     ██║   ██║███████║██║  ██║██║██╔██╗ ██║██║  ███╗    ██║  ██║██║██║        ██║   ██║██║   ██║██╔██╗ ██║███████║██████╔╝ ╚████╔╝ 
-██║     ██║   ██║██╔══██║██║  ██║██║██║╚██╗██║██║   ██║    ██║  ██║██║██║        ██║   ██║██║   ██║██║╚██╗██║██╔══██║██╔══██╗  ╚██╔╝  
-███████╗╚██████╔╝██║  ██║██████╔╝██║██║ ╚████║╚██████╔╝    ██████╔╝██║╚██████╗   ██║   ██║╚██████╔╝██║ ╚████║██║  ██║██║  ██║   ██║   
-╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚═╝╚═╝  ╚═══╝ ╚═════╝     ╚═════╝ ╚═╝ ╚═════╝   ╚═╝   ╚═╝ ╚═════╝ ╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   """
-
-for i in logo.split("\n"):
-    print(textUtil.printg(i, "#ff0000", "#0000ff"))
-print()
-try:
-    with open(file, encoding='latin-1') as f:
-        for i in f.readlines():
-            words.append(i.replace("\n", ""))
-    print("  [~] Loaded succesfully " + str(len(words)) + " passwords.")
-    time.sleep(3)
-except Exception as e:
-    print("  [~] Invalid file.")
+def main():
+    os.system("cls || clear")
     print()
-    exit()
+    printcenter(f"{Fore.YELLOW}{logo}")
+    print()
+    wordlist = input(f"{Fore.RED} [»] {Fore.LIGHTBLUE_EX}Indica el nombre de la wordlist: {Fore.RESET}")
+    if not os.path.isfile(wordlist):
+        os.system("cls || clear")
+        printcenter(f"{Fore.YELLOW}{logo}")
+        print()
+        printcenter(f"{Fore.RESET}La wordlist no ha sido cargada.")
+        print()
+        main()
+    else:
+        os.system("cls || clear")
+        printcenter(f"{Fore.YELLOW}{logo}")
+        print()
+        with open(wordlist, 'r', encoding="latin-1") as f:
+            lines = f.read().splitlines()
+            words.update(lines)
+            print(f"{Fore.LIGHTBLUE_EX}             [LOG]{Fore.RESET} Han sido cargadas {Fore.RED}{len(words)} {Fore.RESET}contraseñas.", end="\r")
+            print()
+            print()
+            hash, salt = "", ""
+            hash = input(f"{Fore.RED} [»] {Fore.LIGHTBLUE_EX}Introduce un hash: {Fore.RESET}")
+            print()
+            if len(hash) == 128:
+                salt = input(f"{Fore.RED} [»] {Fore.LIGHTBLUE_EX}Introduce un salt: {Fore.RESET}")
+            final = bruteforce(hash, salt)
+            if final == hash:
+                os.system("cls || clear")
+                printcenter(f"{Fore.YELLOW}{logo}")
+                print()
+                printcenter(f"{Fore.LIGHTBLUE_EX}[LOG]{Fore.RESET} La contraseña no ha sido encontrada.")
+                time.sleep(3)
+                main()
+            if final != hash:
+                os.system("cls || clear")
+                printcenter(f"{Fore.YELLOW}{logo}")
+                print()
+                printcenter(f"{Fore.LIGHTBLUE_EX}[LOG]{Fore.RESET} La contraseña es -> {Fore.RED}{final}")
+                print()
+                printcenter(f"{Fore.YELLOW}(1) {Fore.RESET}Volver")
+                printcenter(f"{Fore.YELLOW}(2) {Fore.RESET}Salir")
+                print()
+                option = input(f"{Fore.RED} [»] {Fore.LIGHTBLUE_EX}Seleccione una opcion: {Fore.RESET}")
+                if option == "1":
+                    main()
+                elif option == "2":
+                    os.system("cls || clear")
+                    exit()
+                else:
+                    os.system("cls || clear")
+                    printcenter(f"{Fore.RED}{error}")
+                    printcenter(f"{Fore.RESET}¡Debes de seleccionar una opcion valida!")
+                    time.sleep(3)
+                    main()
 
-for _ in range(100): print()
 
-logo = r"""████████▄     ▄████████    ▄█    █▄       ▄████████    ▄████████    ▄█    █▄       ▄████████    ▄████████ 
-███   ▀███   ███    ███   ███    ███     ███    ███   ███    ███   ███    ███     ███    ███   ███    ███ 
-███    ███   ███    █▀    ███    ███     ███    ███   ███    █▀    ███    ███     ███    █▀    ███    ███ 
-███    ███  ▄███▄▄▄      ▄███▄▄▄▄███▄▄   ███    ███   ███         ▄███▄▄▄▄███▄▄  ▄███▄▄▄      ▄███▄▄▄▄██▀ 
-███    ███ ▀▀███▀▀▀     ▀▀███▀▀▀▀███▀  ▀███████████ ▀███████████ ▀▀███▀▀▀▀███▀  ▀▀███▀▀▀     ▀▀███▀▀▀▀▀   
-███    ███   ███    █▄    ███    ███     ███    ███          ███   ███    ███     ███    █▄  ▀███████████ 
-███   ▄███   ███    ███   ███    ███     ███    ███    ▄█    ███   ███    ███     ███    ███   ███    ███ 
-████████▀    ██████████   ███    █▀      ███    █▀   ▄████████▀    ███    █▀      ██████████   ███    ███ """
+##############################################################################################
 
-for i in logo.split("\n"):
-    print(textUtil.printg(i, "#ff0000", "#0000ff"))
-print()
-
-hash, salt = "", ""
-
-hash = input(textUtil.printg("[$] Insert the hash: ", "#ff0000", "#0000ff"))
-if len(hash) == 128:
-    salt = input(textUtil.printg("[$] Insert the salt: ", "#ff0000", "#0000ff"))
-
-logo = r"""
-▓█████▄ ▓█████  ██░ ██  ▄▄▄        ██████  ██░ ██  ██▓ ███▄    █   ▄████                
-▒██▀ ██▌▓█   ▀ ▓██░ ██▒▒████▄    ▒██    ▒ ▓██░ ██▒▓██▒ ██ ▀█   █  ██▒ ▀█▒               
-░██   █▌▒███   ▒██▀▀██░▒██  ▀█▄  ░ ▓██▄   ▒██▀▀██░▒██▒▓██  ▀█ ██▒▒██░▄▄▄░               
-░▓█▄   ▌▒▓█  ▄ ░▓█ ░██ ░██▄▄▄▄██   ▒   ██▒░▓█ ░██ ░██░▓██▒  ▐▌██▒░▓█  ██▓               
-░▒████▓ ░▒████▒░▓█▒░██▓ ▓█   ▓██▒▒██████▒▒░▓█▒░██▓░██░▒██░   ▓██░░▒▓███▀▒ ██▓  ██▓  ██▓ 
- ▒▒▓  ▒ ░░ ▒░ ░ ▒ ░░▒░▒ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░ ▒ ░░▒░▒░▓  ░ ▒░   ▒ ▒  ░▒   ▒  ▒▓▒  ▒▓▒  ▒▓▒ 
- ░ ▒  ▒  ░ ░  ░ ▒ ░▒░ ░  ▒   ▒▒ ░░ ░▒  ░ ░ ▒ ░▒░ ░ ▒ ░░ ░░   ░ ▒░  ░   ░  ░▒   ░▒   ░▒  
- ░ ░  ░    ░    ░  ░░ ░  ░   ▒   ░  ░  ░   ░  ░░ ░ ▒ ░   ░   ░ ░ ░ ░   ░  ░    ░    ░   
-   ░       ░  ░ ░  ░  ░      ░  ░      ░   ░  ░  ░ ░           ░       ░   ░    ░    ░  
- ░                                                                         ░    ░    ░  
-"""
-
-for _ in range(100): print()
-
-for i in logo.split("\n"):
-    print(textUtil.printg(i, "#ff0000", "#0000ff"))
-print("  [~] Wait till the magic is done...")
-
-final = bruteforce(hash, salt)
-if final == hash:
-    print("  [~] Password not found :(")
-if final != hash:
-    print("  [~] Password found: "+final)
+main()
